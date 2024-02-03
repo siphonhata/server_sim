39127a115b684de3629d5dbe790c9e6b7f305b33
siphonhata <siphonhata@gmail.com>
2024-02-03 21:41:38 +0200

    first

---
+++ hello.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const xml2js = require('xml2js');
+
+const app = express();
+const PORT = 3001;
+
+
+app.use(bodyParser.text({ type: 'application/xml' })); // Middleware to parse incoming XML
+ 
+// Route for handling incoming SOAP requests to add two numbers
+app.post('/add', (req, res) => {
+  // Parse the incoming SOAP XML
+  console.log(req.body);
+  xml2js.parseString(req.body, (err, result) => {
+    if (err) {
+      console.error('Error parsing SOAP request:', err);
+      res.status(400).send('Error parsing SOAP request');
+      return;
+    }
+
+    // Extract numbers to be added
+    const num1 = parseInt(result['soap:Envelope']['soap:Body'][0]['Add'][0]['num1'][0]);
+    const num2 = parseInt(result['soap:Envelope']['soap:Body'][0]['Add'][0]['num2'][0]);
+
+    // Perform addition
+    const resultSum = num1 + num2;
+
+    // Prepare SOAP response
+    const responseXML = `
+      <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+        <soap:Body>
+          <AddResponse>
+            <result>${resultSum}</result>
+          </AddResponse>
+        </soap:Body>
+      </soap:Envelope>
+    `;
+
+    // Send SOAP response
+    res.set('Content-Type', 'text/xml');
+    res.send(responseXML);
+  });
+});
+
+// Route for handling incoming SOAP requests to login (UDM)
+app.post('/LGI', (req, res) => 
+  {
+    
+    // Parse the incoming SOAP XML
+    xml2js.parseString(req.body, (err, result) => 
+    {
+      if (err) 
+       {
+            console.error('Error parsing SOAP request:', err);
+            const responseFailedXML = `
+                <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+                    <soap:Body>
+                        <LGIResponse>
+                            <Result>
+                                <ResultCode>1018</ResultCode>
+                                <ResultDesc>Error parsing SOAP request</ResultDesc>
+                            </Result>
+                        </LGIResponse>
+                    </soap:Body>
+                </soap:Envelope>
+            `;
+            res.status(400).send(responseFailedXML);
+            return;
+        }
+  
+      // Extract values to be used
+      const username = result['soap:Envelope']['soap:Body'][0]['LGI'][0]['OPNAME'][0];
+      const pass = result['soap:Envelope']['soap:Body'][0]['LGI'][0]['PWD'][0];
+  
+      // Extract values from bash script
+
+      const udm_username = "udm_username";
+      const udm_password = "udm_password";
+      // Perform login
+      
+      if (username === udm_username && pass === udm_password)
+      {
+        console.log("username and pass are correct")
+        const responseXML = `
+        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+              <soap:Body>
+                <LGIResponse>
+                    <Result>
+                        <ResultCode>0</ResultCode>
+                        <ResultDesc>Operation is successful</ResultDesc>
+                    </Result>
+                </LGIResponse>
+              </soap:Body>
+            </soap:Envelope>
+          `;
+  
+        // Send SOAP response
+        res.set('Content-Type', 'application/xml');
+        res.send(responseXML);
+        return;
+      }
+      else
+      {
+        console.log("username and pass dont match")
+        const responseFailedXML = `
+          <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+            <soap:Body>
+              <LGIResponse>
+                <Result>
+                  <ResultCode>1018</ResultCode>
+                  <ResultDesc>Username/Password doesn&apos;t match</ResultDesc>
+                </Result>
+              </LGIResponse>
+            </soap:Body>
+          </soap:Envelope>
+        `;
+        res.status(200).send(responseFailedXML);
+        return;
+      }
+    });
+  });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+    
+});
---
+++ variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set environment variables
+export UDM_username= udm_username
+export UDM_password= udm_password
